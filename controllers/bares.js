c6d607bdd3f60c0ba09f187b97e6edc5efe7ba90
NadiaCorrea <ncormor536@gmail.com>
2022-10-30 13:34:07 +0100

    cervezas done

--- controllers/bares.js
+++ controllers/bares.js
@@ -1,38 +1,60 @@
-const db = require('../models/db')
+let db = require('../models/db');
+db = db.connect('./data', ['bares']);
+const url = require('url');
+const { response } = require('express');
 
-//let bares = [{id:1, name:"Gambrinus", location:"Sevilla"}, {id:2, name:"La esquina", location:"Brenes"}];
-// const lastBar = bares.slice(-1);
-// const id = lastBar.id;
+//Properties of bares - id, name, address, location
 
+//gets all bares if petition doesn't have query params. if petition have query params but the location
+// it gets all bares which location is Sevilla and meet the other params.
 function getBares(req, res){
-    //res.json(bares);
-    res.json(db.bares.find());
+    let query = url.parse(req.url,true).query;
+    if (Object.keys(query).length == 0){
+        res.json(db.bares.find());
+    } else{
+        if (query.localidad === undefined){
+            query.localidad = "Sevilla";
+        }
+        //console.log(query);
+        res.json(db.bares.find(query));
+    }
 };
 
+/*Un recurso GET que recibe un parámetro 
+y devuelve el documento con dicho id.*/
 function getBar(req, res){
-
+    let idToSearch = req.params.id; 
+    let barToSearch = {_id:idToSearch};
+    res.json(db.bares.findOne(barToSearch));
 };
 
+/*Un recurso POST para 
+crear nuevos documentos de ese recurso.*/
 function addBar(req, res){
-    bares.push(req.body);
+    let newBar = req.body;
+    db.bares.save(newBar);
     res.json({ mensaje: '¡Bar añadido!' }); 
 };
 
+/*Un recurso DELETE para eliminar documentos.*/
 function deleteBar(req, res)   { 
-let barToSearch = req.params.name;
-bares = arrayRemove(bares,barToSearch);
-res.json({ mensaje: 'Bar eliminado.'}) 
+    let barToSearch = req.params.id;
+    let barToDelete = {_id: barToSearch};
+    db.bares.remove(barToDelete);
+    res.json({ mensaje: 'Bar eliminado.'}) 
 };
 
-function arrayRemove(array, value) {
-return array.filter(function(element){
-    return element.name != value;
-});
-};
-
-
+/*Un recurso PUT para editar documentos.*/
 function editBar(req, res){
-
+    let itemToUpdate = req.params.id;
+    let newBar = req.body;
+    let barToSearch = {_id: itemToUpdate};
+    let options = {
+        multi: false,
+        upsert: false
+    }
+    db.bares.update(barToSearch, newBar, options);
+    res.json({ mensaje: 'Cerveza actualizada!'});
 };
 
 module.exports = {getBares, getBar, addBar, deleteBar, editBar};
