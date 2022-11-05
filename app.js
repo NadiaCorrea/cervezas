d388459825f7fa1bc467a44e3ce9ce648b0da363
NadiaCorrea <ncormor536@gmail.com>
2022-11-05 17:52:30 +0100

    adding changes in app.js

--- app.js
+++ app.js
@@ -1,27 +1,21 @@
+const { connect } = require('diskdb');
 const express = require('express');
-const app = express();
+const { dbConnection } = require('./database/config');
+require('dotenv').config();
 
-
-
-// establecemos nuestro puerto
-const port = 3000;
-
+const app = express()
 // require('./db')
-const cervezas = require('./routes/cervezas');
-const bares = require('./routes/bares');
+const cervezas = require('./routes/cervezas')
 
+// DATABASE CONNECTION
+async function connectAtlas(){
+    await dbConnection()
+}
+connectAtlas()
 //MIDDLEWARE
 app.use(express.json())
 
 //ROUTES
-app.use('/cervezas', cervezas);
-app.use('/bares', bares);
+app.use('/cervezas', cervezas)
 
-// app.use(express.json());
-// app.use('/cervezas', require('./routes/cervezas.js'));
-// app.use('/bares', require('./routes/bares.js'));
-
-
-// iniciamos nuestro servidor
-app.listen(port)
-console.log('API escuchando en el puerto ' + port);
+app.listen(process.env.PORT)
